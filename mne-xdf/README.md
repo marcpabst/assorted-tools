c31e2b70c48f75522ecf7a66e946ab0d7dbf178e
Marc Pabst <marcpabst@users.noreply.github.com>
2025-11-28 21:41:16 +0000

    feat: Add mne-xdf package with CI workflow
    - Create mne-xdf package structure based on bids-like
    - Add GitHub Actions workflow for building and publishing mne-xdf
    - Configure workflow for XDF file format support for MNE-Python

---
+++ mne-xdf/README.md
@@ -0,0 +1,3 @@
+# MNE-XDF
+
+A Python library for importing XDF files for use with MNE-Python. This package provides utilities to read and convert XDF (eXtensible Data Format) files into MNE-Python compatible data structures.
---
+++ mne-xdf/__init__.py
@@ -0,0 +1,8 @@
+"""
+MNE-XDF: Import XDF files for use with MNE-Python.
+
+This package provides utilities to read XDF (eXtensible Data Format) files
+and convert them into MNE-Python compatible data structures.
+"""
+
+__version__ = "0.1.0"
